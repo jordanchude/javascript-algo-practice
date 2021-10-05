5f530fadde427f1a836cd158132dd84d86f57445
Jordan <jordan@chude.co>
2021-10-05 10:09:42 -0400

    index of leetcode

--- 704-binary-search-leetcode.js
+++ 704-binary-search-leetcode.js
@@ -13,5 +13,8 @@ var search = function(nums, target) {
 };
 
 // INDEX OF
+var search = function(nums, target) {
+    return nums.indexOf(target);
+}
 
-console.log(search([-1,0,3,5,9,12], 10))
+console.log(search([-1,0,3,5,9,12], 9))
