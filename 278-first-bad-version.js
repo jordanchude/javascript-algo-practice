e1a78f2d9e4c75a9916d0aef8213f8208278623f
Jordan <jordan@chude.co>
2021-10-06 10:04:09 -0400

    linear solution addition 278

--- 278-first-bad-version.js
+++ 278-first-bad-version.js
@@ -4,12 +4,45 @@
 
 // You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
-// PSEUDOCODE
-// LINEAR
+// LINEAR PSEUDOCODE
 // var solution = function(isBadVersion) {
 //     
 //     return function(n) {
 //     loop through array n times
 //     if array contains bad version, return index of bad version
+//     };
+// };
+
+
+// LINEAR SOLUTION
+// var solution = function(isBadVersion) {
+//     /**
+//      * @param {integer} n Total versions
+//      * @return {integer} The first bad version
+//      */
+//     return function(n) {
+//         for (let i = 0; i < n; i++)  {
+//             if (isBadVersion(i)) {
+//                 return i;
+//             }
+//         }
+//         return n;
+//     };
+// };
+
+// ITERATIVE PSEUDOCODE
+// var solution = function(isBadVersion) {
+    
+//     return function(n) {
+//       create left variable
+//       create right variable
+//       while left is bigger than or equal to right
+//         create mid variable
+//         if middle of array isBadVersion(n)
+//             return index of middle of array
+//         else if middle of array is bigger than n
+//             right equals mid minus 1
+//         else 
+//             left mid plus 1
 //     };
 // };
