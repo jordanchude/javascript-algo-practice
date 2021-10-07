42c382f61561d8609298807d89acb12a22f25f91
Jordan <jordan@chude.co>
2021-10-07 10:11:25 -0400

    2nd solution

--- 35-search-position.js
+++ 35-search-position.js
@@ -27,13 +27,13 @@
 Inputs: sorted array, target value
 Outputs: Location of target value in sorted array
 
-PSEUDOCODE
+1ST PSUEDOCODE
 FUNCTION searchInsert EQUALS function(nums, target) {
     FOR LOOP through array
         IF target is in array 
             return index
     END LOOP 
-    
+
     insert target into array
     sort array
     return index of target
@@ -41,7 +41,7 @@ FUNCTION searchInsert EQUALS function(nums, target) {
 
 */
 
-// INEFFICIENT SOLUTION
+// 1ST SOLUTION
 // var searchInsert = function(nums, target) {
 //     for (let i = 0; i < nums.length; i++) {
 //        if (nums[i] == target) {
@@ -52,5 +52,50 @@ FUNCTION searchInsert EQUALS function(nums, target) {
 //     nums.sort(function(a, b) {return a-b})
 //     return nums.indexOf(target);
 // };
+
+/* 2ND PSUEDOCODE
+FUNCTION  searchInsert EQUALS function(nums, target) {
+    LET left EQUAL 0
+    LET right EQUAL nums.length - 1
+
+    WHILE  left is bigger than right
+        LET middle EQUAL left + ROUNDED right MINUS left DIVDED BY 2
+
+        IF middle index equals target
+            return middle index
+        ELSE IF target is lower than middle
+            right equals middle - 1
+        ELSE 
+            left equals middle + 1
+    END WHILE
+
+    insert target into array
+    sort array
+    return index of target
+};
+
+*/
+
+// 2ND SOLUTION
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let middle = left + Math.floor((right - left / 2))
+
+        if (nums[middle] == target) {
+            return nums.indexOf(target);
+        } else if (target < nums[middle]) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+
+    nums.push(target);
+    nums.sort(function(a, b) {return a-b})
+    return nums.indexOf(target);
+};
 
 console.log(searchInsert([1,3,5,6], 5))
