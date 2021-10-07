f45f23e6aba08cd9113cfd5758de5cb280dac0ae
Jordan <jordan@chude.co>
2021-10-07 08:54:09 -0400

    complete 278 first bad version

--- 278-first-bad-version.js
+++ 278-first-bad-version.js
@@ -70,3 +70,27 @@
 //         return lower;
 //     };
 // };
+
+// ITERATIVE SOLUTION [ACCEPTED]
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1
+        let right = n
+        
+        while (left < right) {
+            let middle = left + Math.floor((right - left) / 2)
+            
+            if (isBadVersion(middle)) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        
+        return left;
+    };
+};
