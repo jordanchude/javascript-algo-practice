ff8b77c26cec1c94d6659ae88cebb7b77999c907
Jordan <jordan@chude.co>
2021-10-19 20:44:21 -0400

    post solution check

--- binary-search-practice/sqrt(x).js
+++ binary-search-practice/sqrt(x).js
@@ -1,7 +1,20 @@
-/**
- * @param {number} x
- * @return {number}
- */
+// @param {number} x
+// @return {number}
+
  var mySqrt = function(x) {
-    
+    var start = 0;
+    var end = Number.MAX_SAFE_INTEGER;
+
+    while (start <= end) {
+        var middle = Math.floor((start + end)/2)
+        var num = middle * middle;
+
+        if (x < 2) {
+            return x;
+        } else if (num > x) {
+            
+        }
+    }
 };
+
+console.log(mySqrt(16))
