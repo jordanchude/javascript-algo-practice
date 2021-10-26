4602f9a9121be9789e06997fe0af63e8734d6e15
Jordan <jordan@chude.co>
2021-10-26 08:26:29 -0400

    understand the problem answers

--- binary-search-practice/sqrt(x).js
+++ binary-search-practice/sqrt(x).js
@@ -1,9 +1,16 @@
-// Understand the Problem
-// 1. Can I restate the problem in my own words?
-// 2. What are the inputs that go into the problem?
-// 3. What are the outputs that should come from the solution to the problem?
-// 4. Can the outputs be determined from the inputs?
-// 5. How should I label the important pieces of data that are a part of the problem?
+/* 
+Understand the Problem
+1. Can I restate the problem in my own words?
+- What closest rounded number multiplied by itself gives us x? 
+2. What are the inputs that go into the problem?
+- The squared output of the problem.
+3. What are the outputs that should come from the solution to the problem?
+- The rounded square root of x.  
+4. Can the outputs be determined from the inputs?
+- Yes.
+5. How should I label the important pieces of data that are a part of the problem?
+- start, end, middle
+*/ 
 
 // @param {number} x
 // @return {number}
@@ -12,20 +19,7 @@
     var start = 0;
     var end = Number.MAX_SAFE_INTEGER;
 
-    while (start <= end) {
-        var middle = (start + end)/2
-        var num = middle * middle;
-
-        if (x < 2) {
-            return x;
-        } else if (num === x) {
-            return middle;
-            end = middle - 1;
-        } else if (num < x) {
-            start = middle + 1;
-        }
-    }
-    return middle;
+    
 };
 
 console.log(mySqrt(50))
