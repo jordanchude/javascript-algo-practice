824a4737290a7010401a77bc23df580fc5171222
Jordan <jordan@chude.co>
2021-10-27 08:58:09 -0400

    rotated array understand the problem

--- binary-search-practice/search-in-a-rotated-sorted-array.js
+++ binary-search-practice/search-in-a-rotated-sorted-array.js
@@ -6,3 +6,21 @@ Given the array nums after the possible rotation and an integer target, return t
 
 You must write an algorithm with O(log n) runtime complexity. */
 
+/* 
+Understand the Problem
+1. Can I restate the problem in my own words?
+In an unsorted array based on a pivot value, is target in nums?
+
+2. What are the inputs that go into the problem?
+- An unsorted array based on a pivot value.
+- A target value to check our array for.
+
+3. What are the outputs that should come from the solution to the problem? 
+The index of the target value in the nums array.
+
+4. Can the outputs be determined from the inputs?
+Yes.
+
+5. How should I label the important pieces of data that are a part of the problem?
+Label the pivot, the target, and the num as their own variables.
+*/ 
