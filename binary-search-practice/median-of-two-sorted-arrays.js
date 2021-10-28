991b40b5b1b50653bdfa243bd842f7fe753133c3
Jordan <jordan@chude.co>
2021-10-28 11:50:32 -0400

    understand the problem v1

--- binary-search-practice/median-of-two-sorted-arrays.js
+++ binary-search-practice/median-of-two-sorted-arrays.js
@@ -6,8 +6,17 @@ The overall run time complexity should be O(log (m+n)).
 
 /* UNDERSTAND THE PROBLEM
 1. Can I restate the problem in my own words?
+- Given two sorted arrays, once merged, return the median.
+
 2. What are the inputs that go into the problem?
+- Two sorted arrays, nums1 and nums2.
+
 3. What are the outputs that should come from the solution to the problem?
+- The median of the two merged arrays.
+
 4. Can the outputs be determined from the inputs?
+- Yes.
+
 5. How should I label the important pieces of data that are a part of the problem?
+- Label the first array, second array, and median.
 */
