b54ce0d0d8d16440a01fae3df053d5cf7c1cfa36
Jordan <jordan@chude.co>
2021-10-28 11:30:26 -0400

    rotated sorted array solution v2

--- binary-search-practice/search-in-a-rotated-sorted-array.js
+++ binary-search-practice/search-in-a-rotated-sorted-array.js
@@ -112,7 +112,29 @@ END FUNCTION
 // OPTIMIZED SOLUTION
 var search = function(nums, target) {
     var start = 0;
+    var end = nums.length - 1;
+
+    while (start <= end) {
+        var mid = Math.floor((start + end) /2);
+
+        if (target === nums[mid]) {
+            return mid;
+        } else if (nums[mid] >= nums[start]) {
+            if (target >= nums[start] && target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (target <= nums[end] && target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
 };
 
 
-console.log(search([3,1], 1));
+console.log(search([5,1,3], 3));
