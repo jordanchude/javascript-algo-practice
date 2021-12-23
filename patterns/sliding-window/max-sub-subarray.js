7ae7a18bea44d8afbeb5470f0b65c372d234ebd1
Jordan <jordan@chude.co>
2021-12-23 01:24:23 -0500

    patterns practice

---
+++ patterns/sliding-window/max-sub-subarray.js
@@ -0,0 +1,17 @@
+const max_sub_array_of_size_k = function(k, arr) {
+    // array to store max sub array
+    const result = []
+
+    // initialize window sum to compare
+    let windowSum = 0.0
+
+    //initialize beginning of search window
+    let windowStart = 0;
+
+
+    return -1;
+};
+
+
+const result = max_sub_array_of_size_k(3, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
+console.log(`Subarray with maximum sum is: ${result}`);
