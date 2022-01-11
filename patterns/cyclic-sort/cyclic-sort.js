1865c82959aeeea886025bad8566b14fca3ca218
Jordan <jordan@chude.co>
2022-01-11 10:04:56 -0500

    space complexity and time complexity additions to cyclic sort

--- patterns/cyclic-sort/cyclic-sort.js
+++ patterns/cyclic-sort/cyclic-sort.js
@@ -47,3 +47,6 @@ const cyclic_sort = function(nums) {
   console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`)
   console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`)
   console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`)
+
+  // Time complexity: O(n)
+  // Space complexity: O(1)
