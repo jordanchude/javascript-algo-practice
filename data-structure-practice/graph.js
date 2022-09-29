86d6714cb0c50cbf5186934ea552883944a3f6cc
Jordan <jordan@chude.co>
2022-09-28 20:07:16 -0400

    breadth first search addition

--- data-structure-practice/graph.js
+++ data-structure-practice/graph.js
@@ -1,22 +1,56 @@
-// DEPTH FIRST SEARCH
-const depthFirstPrint = (graph, source) => {
-    // initialize starting node with source node
-    const stack = [ source ];
+// DEPTH FIRST SEARCH (ITERATIVE)
+// stack: remove from end, add to end (LIFO)
+// const depthFirstPrint = (graph, source) => {
+//     // initialize starting node with source node
+//     const stack = [ source ];
 
-    while (stack.length > 0) {
-        const current = stack.pop();
-        console.log(current);
+//     while (stack.length > 0) {
+//         const current = stack.pop();
+//         console.log(current);
 
-        // iterate through neighbors of current node
+//         // iterate through neighbors of current node
+//         for (let neighbor of graph[current]) {
+//             stack.push(neighbor);
+//         }
+//     }
+// }
+
+// DEPTH FIRST SEARCH (RECURSIVE)
+// const depthFirstPrint = (graph, source) => {
+   
+//     console.log(source);
+
+//     // base case isn't explicit
+//     // base case is if the array to iterate is empty
+//    for (let neighbor of graph[source]) {
+//     depthFirstPrint(graph, neighbor)
+//    }
+// }
+
+// BREADTH FIRST SEARCH
+// queue: remove from front, add to back (FIFO)
+const breadthFirstPrint = (graph, source) => {
+    const queue = [ source ];
+
+    while (queue.length > 0) {
+        // remove first element
+        let current = queue.shift();
+        console.log(current)
+
         for (let neighbor of graph[current]) {
-            stack.push(neighbor);
+            // add neighors to end of queue
+            queue.push(neighbor);
         }
     }
 }
 
 
+// Why Use A HashMap to represent a graph?
+// constant time
+// lookup data structure
+// key value pairings
 const graph = {
-    a: ['c', 'b'],
+    a: ['b', 'c'],
     b: ['d'],
     c: ['e'],
     d: ['f'],
@@ -24,5 +58,5 @@ const graph = {
     f: []
 }
 
-depthFirstPrint(graph, 'a'); // abdfce
-// breadthFirstPrint(graph, 'a') // acbedf
+// depthFirstPrint(graph, 'a'); // abdfce
+breadthFirstPrint(graph, 'a') // acbedf
